1f7ac4c477dc35fb5a6f873483a3ed53744bf667
TimePath <andrew.hardaker1995@gmail.com>
2014-04-18 13:58:18 +1000

    Array support for Struct

--- src/com/timepath/io/struct/Struct.java
+++ src/com/timepath/io/struct/Struct.java
@@ -3,6 +3,7 @@ package com.timepath.io.struct;
 import com.timepath.io.OrderedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.*;
 import java.util.logging.Level;
@@ -32,11 +33,18 @@ public class Struct {
             unpack(out, new OrderedInputStream(new ByteArrayInputStream(b)));
         } catch(IOException ex) {
             LOG.log(Level.SEVERE, null, ex);
+        } catch(IllegalArgumentException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        } catch(IllegalAccessException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        } catch(InstantiationException ex) {
+            LOG.log(Level.SEVERE, null, ex);
         }
     }
 
-    public static void unpack(Object out, OrderedInputStream is) {
-        Field[] fields = out.getClass().getDeclaredFields();
+    public static void unpack(Object instance, OrderedInputStream is)
+        throws IOException, IllegalArgumentException, IllegalAccessException, InstantiationException {
+        Field[] fields = instance.getClass().getDeclaredFields();
         // Filter
         ArrayList<Field> al = new ArrayList<Field>(fields.length);
         for(Field ref : fields) {
@@ -53,68 +61,69 @@ public class Struct {
             }
         });
         // Iterate
-        try {
-            for(Field field : al) {
-                StructField meta = field.getAnnotation(StructField.class);
-                is.skipBytes(meta.skip());
-                Object var;
-                Primitive primitive = primitiveTypes.get(field.getType().getName());
-                if(primitive == null) {
-                    primitive = Primitive.OBJECT;
-                }
-                switch(primitive) {
-                    case BOOLEAN:
-                        var = is.readBoolean();
-                        break;
-                    case BYTE:
-                        var = is.readByte();
-                        break;
-                    case CHAR:
-                        var = is.readChar();
-                        break;
-                    case SHORT:
-                        var = is.readShort();
-                        break;
-                    case INT:
-                        var = is.readInt();
-                        break;
-                    case LONG:
-                        var = is.readLong();
-                        break;
-                    case FLOAT:
-                        var = is.readFloat();
-                        break;
-                    case DOUBLE:
-                        var = is.readDouble();
-                        break;
-                    case STRING:
-                        var = is.readString(meta.limit());
-                        break;
-                    case OBJECT:
-                        LOG.log(Level.FINE, "Instantiating {0}", field);
-                        var = field.getType().newInstance(); // Fatal if fails
-                        Struct.unpack(var, is);
-                        break;
-                    default:
-                        continue;
-                }
-                try {
-                    field.setAccessible(true);
-                    field.set(out, var);
-                } catch(IllegalArgumentException ex) {
-                    LOG.log(Level.SEVERE, null, ex);
-                } catch(IllegalAccessException ex) {
-                    LOG.log(Level.SEVERE, null, ex);
-                }
-
-            }
-        } catch(IOException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-        } catch(InstantiationException ex) {
-            LOG.log(Level.SEVERE, null, ex);
-        } catch(IllegalAccessException ex) {
-            LOG.log(Level.SEVERE, null, ex);
+        for(Field field : al) {
+            Object var = readField(instance, field, is);
+            field.set(instance, var);
         }
+    }
+
+    private static void readArray(Object ref, Field field, OrderedInputStream is, int depth)
+        throws IOException, InstantiationException, IllegalAccessException {
+        StructField meta = field.getAnnotation(StructField.class);
+        int dimensions = field.getType().getName().lastIndexOf('[');
+        Class<?> elemType = field.getType();
+        for(int i = 0; i < dimensions + 1; i++) {
+            elemType = elemType.getComponentType();
+        }
+        Primitive primitive = primitiveTypes.get(elemType.getName());
+
+        for(int i = 0; i < Array.getLength(ref); i++) {
+            Object elem = Array.get(ref, i);
+            if(depth == dimensions) { // Not a nested array
+                if(primitive != null) { // Element is a primitive type
+                    elem = primitive.read(is, meta.limit());
+                } else {
+                    if(elem == null) { // Instantiate if needed
+                        LOG.log(Level.FINE, "Instantiating {0}", field);
+                        elem = elemType.newInstance();
+                    }
+                    Struct.unpack(elem, is);
+                }
+                Array.set(ref, i, elem);
+            } else {
+                readArray(elem, field, is, depth + 1);
+            }
+        }
+    }
+
+    private static Object readField(Object instance, Field field, OrderedInputStream is)
+        throws IOException, IllegalArgumentException, IllegalAccessException, InstantiationException {
+        StructField meta = field.getAnnotation(StructField.class);
+        is.skipBytes(meta.skip());
+
+        Object ref;
+        Primitive primitive = primitiveTypes.get(field.getType().getName());
+        if(primitive != null) { // Field is a primitive type
+            return primitive.read(is, meta.limit());
+        } else if(field.getType().isArray()) { // Field is an array
+            ref = field.get(instance);
+            if(ref == null) { // Check if instantiated
+                throw new InstantiationException("Cannnot instantiate array of unknown length");
+            }
+            readArray(ref, field, is, 0);
+        } else { // Field is a regular Object
+            ref = field.get(instance);
+            if(ref == null) { // Instantiate if needed
+                LOG.log(Level.FINE, "Instantiating {0}", field);
+                ref = field.getType().newInstance();
+            }
+            Struct.unpack(ref, is);
+            boolean accessible = field.isAccessible();
+            field.setAccessible(true);
+            field.set(instance, ref);
+            field.setAccessible(accessible);
+        }
+        return ref;
     }
 
     private Struct() {
@@ -129,6 +138,41 @@ public class Struct {
 
         private Primitive(String type) {
             this.type = type;
+        }
+
+        /**
+         * Read a primitive
+         * <p>
+         * @param is
+         * @param limit
+         *              <p>
+         * @return The primitive
+         * <p>
+         * @throws IOException
+         */
+        Object read(OrderedInputStream is, int limit) throws IOException {
+            switch(this) {
+                case BOOLEAN:
+                    return is.readBoolean();
+                case BYTE:
+                    return is.readByte();
+                case CHAR:
+                    return is.readChar();
+                case SHORT:
+                    return is.readShort();
+                case INT:
+                    return is.readInt();
+                case LONG:
+                    return is.readLong();
+                case FLOAT:
+                    return is.readFloat();
+                case DOUBLE:
+                    return is.readDouble();
+                case STRING:
+                    return is.readString(limit);
+                default:
+                    return null;
+            }
         }
 
     }
