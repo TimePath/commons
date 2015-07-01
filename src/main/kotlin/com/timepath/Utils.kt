027ef67851e40b24ee63ba17128a05f008d31028
TimePath <andrew.hardaker1995@gmail.com>
2015-07-01 15:37:32 +1000

    JRE<8 support for toUnsigned()

--- src/main/kotlin/com/timepath/Utils.kt
+++ src/main/kotlin/com/timepath/Utils.kt
@@ -7,9 +7,13 @@ public inline fun <T : Any, R> T.with(f: T.() -> R): T = let { f(); it }
 
 public inline fun ExecutorService.use<T>(body: ExecutorService.() -> T): T = body().with { shutdown() }
 
-public fun Byte.toUnsigned(): Int = java.lang.Byte.toUnsignedInt(this)
-public fun Short.toUnsigned(): Int = java.lang.Short.toUnsignedInt(this)
-public fun Int.toUnsigned(): Long = java.lang.Integer.toUnsignedLong(this)
-public fun Long.toUnsigned(): BigInteger = java.lang.Long.toUnsignedString(this) let { BigInteger(it) }
+public fun Byte.toUnsigned(): Int = this.toInt() and 0xff
+public fun Short.toUnsigned(): Int = this.toInt() and 0xffff
+public fun Int.toUnsigned(): Long = this.toLong() and 0xffffffffL
+public fun Long.toUnsigned(): BigInteger {
+    val quot = (this ushr 1) / 5
+    val rem = this - quot * 10
+    return BigInteger(quot.toString() + rem)
+}
 
 public fun log(n: Int, i: Int): Int = (Math.log(i.toDouble()) / Math.log(n.toDouble())).toInt()
